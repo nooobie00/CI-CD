0255543563e6f9b0eaaa09e1ba04fa6d6d7da84d
cip <samuelfortunate098@gmail.com>
2026-04-29 23:57:30 -0700

    Added healthcheck

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.static('dist'))
 app.get('/version', (req, res) => {
   res.send('1')
 })
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 const start = async () => {
   await app.listen(PORT)
